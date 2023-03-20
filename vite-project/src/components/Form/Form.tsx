4407775526a6581c57b503e1b7e625ea55b309bd
Yahor Litavar <sergioegor21@gmail.com>
2023-03-20 22:10:20 +0300

    feat: add limits for post

--- vite-project/src/components/Form/Form.tsx
+++ vite-project/src/components/Form/Form.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import TweetTextarea from 'tweet-textarea-react'
+import { LIMIT } from '../../data/limits';
 import patterns from '../../data/patterns';
 import { IPost } from "../../data/types";
 import "./Form.scss"
@@ -13,6 +14,8 @@ interface IProps {
 const Form = ({post, posts, setPosts}:IProps) => {
   const [text, setText] = useState<string>('');
   const [title, setTitle] = useState<string>('');
+  const [titleSize, setTitleSize] = useState<number>(0);
+  const [textSize, setTextSize] = useState<number>(0);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -50,13 +53,35 @@ const Form = ({post, posts, setPosts}:IProps) => {
         type="text" 
         name="title"  
         value={title} 
-        onChange={(e) => setTitle(e.currentTarget.value)}
+        onChange={(e) => {
+          const value = e.currentTarget.value;
+          if (value.length > LIMIT.POST_TITLE) return;
+          setTitleSize(value.length);
+          setTitle(e.currentTarget.value);
+        }}
       />
+      <label>
+        {titleSize} / {LIMIT.POST_TITLE}
+      </label>
       <TweetTextarea 
         value={text}
         defaultValue={post ? post.text : ''}
-        onTextUpdate={(e) => setText(e.detail.currentText)} />
-      <button>Submit</button>
+        onTextUpdate={(e) => {
+          const value = e.detail.currentText;
+          if (value.length > LIMIT.POST_TEXT) return;
+          setTextSize(e.detail.currentText.length);
+          setText(e.detail.currentText);
+        }} 
+      />
+      <label>
+        {textSize} / {LIMIT.POST_TEXT}
+      </label>
+      <button 
+        disabled={
+          titleSize < LIMIT.MIN_POST_TITLE || 
+          textSize < LIMIT.MIN_POST_TEXT
+        }
+      >Submit</button>
     </form>
   );
 };
---
+++ vite-project/src/data/limits.ts
@@ -0,0 +1,6 @@
+export const LIMIT = {
+  POST_TITLE: 10,
+  POST_TEXT: 400,
+  MIN_POST_TITLE: 1,
+  MIN_POST_TEXT: 1
+}
